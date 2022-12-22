bd0ee7668809e70bdcf9b86a6ccd1fa748a70fed
tequilex <dron2010xxx@mail.ru>
2022-12-22 18:07:53 +0300

    build: fix forms

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ import Button from "../UI/Button/Button"
 import { connect } from "react-redux"
 import { useHistory } from "react-router-dom";
 import { saveCard, getCard} from '../../store/actions'
-import { Formik, Form, Field, ErrorMessage } from "formik"
+import { useFormik } from "formik"
 import * as Yup from 'yup'
 import './Profile.scss'
 
@@ -16,6 +16,17 @@ function Profile({saveCard, getCard}) {
 
   const saveCardData = localStorage.getItem('cardData')
   const data = JSON.parse(saveCardData)
+
+  const placeHolder = (preview, value, simbol) => {
+    Array.from(value).forEach(el => preview = preview.replace(simbol, el) )
+  
+    return preview.replace(/(\d?)\D+$/, '$1')
+  }
+
+  const regName = (value) => value.replace(/\d/g, '')
+  const regCVC = (value) => value.replace(/\D/g, '').substr(0, 3)
+  const regDate = (value) => placeHolder('**/**', value.replace(/\D/g, ''), '*')
+  const regCard = (value) => placeHolder('**** **** **** ****', value.replace(/\D/g, ''), '*')
 
   const [cardNumber, setCardNumber] = useState(data?.cardNumber)
   const [expiryDate, setExpiryDate] = useState(data?.expiryDate)
@@ -40,79 +51,87 @@ function Profile({saveCard, getCard}) {
       saveCard(cardNumber, cardName, expiryDate, cvc)
   }
 
-
-
-  return (
-    <Formik
-    initialValues={{
+  const formik = useFormik({
+    initialValues: {
       cardName: data?.cardName,
       cardNumber: data?.cardNumber,
       expiryDate: data?.expiryDate,
       cvc: data?.cvc
-    }}
-    validationSchema = {Yup.object({
+    },
+    validationSchema: Yup.object({
       cardName: Yup.string()
-      .required('Введите ваше имя'),
+      .required('Введите Ваше имя и фамилию'),
       cardNumber: Yup.string()
-      .required('Введите номер карты')
-      .matches(/\d{13,18}/, 'не верный формат'),
+      .min(16, 'Проверьте номер карты')
+      .required('Поле обязательно для заполнения'),
       expiryDate: Yup.string()
       .min(4, 'Проверьте дату')
       .required('Поле обязательно для заполнения'),
       cvc: Yup.string()
-      .min(3, 'Проверьте ваш CVC код')
-      .matches(/\d{3,5}/, 'не верный формат')
-      .required('Поле обязательно')
-    })}
-    >
+      .min(3, 'Проверьте Ваш CVC код')
+      .required('Поле обязательно для заполнения')
+    }),
+    onSubmit: values => handleSubmit(values)
+  })
+
+
+
+  return (
     <div className="Page__overlay" onClick={() => {history.push('/')}}>
     <div className="profile" onClick={e => e.stopPropagation()}>
-      <Form className="profile__form" onSubmit={handleSubmit}>
+      <form className="profile__form" onSubmit={handleSubmit}>
         <div className="profile__title">Профиль</div>
         <div className="profile__subtitle">Введите платежные данные</div>
         <div className="profile-form__wrapper">
           <div className="form__info">
             <div className="input">
               <label htmlFor="" className="input__title">Имя владельца</label>
-              <Field
-                as={Input}
+              <Input
                 type="cardName"
                 name="cardName"
                 placeholder="ANDREY IVANOV"
+                onChange={(e) => {formik.handleChange(e); setCardName(regName(e.target.value))}}
+                onBlur={formik.handleBlur} 
+                value={cardName}
               />
-              <ErrorMessage name="cardName"/>
+              {formik.touched.cardName && formik.errors.cardName ? (<div>{formik.errors.cardName}</div>) : null}
             </div>
             <div className="input">
               <label htmlFor="" className="input__title">Номер карты</label>
-              <Field
-                as={Input}
+              <Input
                 type="card"
                 name="cardNumber"
                 placeholder="0000 0000 0000 0000"
-                onChange={(e) => setCardName(e.target.value)}
+                onChange={(e) => {formik.handleChange(e); setCardNumber(regCard(e.target.value))}}
+                onBlur={formik.handleBlur} 
+                value={cardNumber}
               />
-              <ErrorMessage name="cardNumber"/>
+              {formik.touched.cardNumber && formik.errors.cardNumber ? (<div>{formik.errors.cardNumber}</div>) : null}
               </div>
               <div className="card-row">
                 <div className="input">
                   <label htmlFor="" className="input__title">MM/YY</label>
-                  <Field
-                    as={Input}
+                  <Input
                     type="text"
                     name="expiryDate"
                     placeholder="00/00"
+                    onChange={(e) => {formik.handleChange(e); setExpiryDate(regDate(e.target.value)) }}
+                    onBlur={formik.handleBlur} 
+                    value={expiryDate}
                   />
-                  <ErrorMessage name="expiryDate"/>
+                  {formik.touched.expiryDate && formik.errors.expiryDate ? (<div>{formik.errors.expiryDate}</div>) : null}
                 </div>
                 <div className="input">
                   <label htmlFor="" className="input__title">CVC</label>
-                  <Field
-                    as={Input}
+                  <Input
                     type="text"
                     name="cvc"
                     placeholder="***"
+                    onChange={(e) => {formik.handleChange(e); setCvc(regCVC(e.target.value)) }}
+                    onBlur={formik.handleBlur} 
+                    value={cvc}
                   />
-                  <ErrorMessage name="cvc"/>
+                  {formik.touched.cvc && formik.errors.cvc ? (<div>{formik.errors.cvc}</div>) : null}
                 </div>
               </div>
           </div>
@@ -129,10 +148,9 @@ function Profile({saveCard, getCard}) {
         </div>
         </div>
         <Button type="submit">Сохранить</Button>
-      </Form>
+      </form>
     </div>
     </div>  
-  </Formik>
   )
 }
 
