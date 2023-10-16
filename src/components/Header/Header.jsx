184dc135543d17a0eae4eaaeb941d468ef4eff50
tequilex <dron2010xxx@mail.ru>
2023-10-16 17:57:21 +0300

    add adaptive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,29 @@ import logo from '../../assets/img/logo.svg'
 import {Link} from 'react-router-dom'
 import './Header.scss'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 function Header(events) {
   const {clickNavItem} = events
+  const [isActive, setIsActive] = useState('')
+  const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+  const updateMenu = () => {
+    if(!isMenuClicked) {
+      setIsActive('active-nav')
+      setIsMenuClicked(!isMenuClicked)
+    } else {
+      setIsActive('')
+      setIsMenuClicked(!isMenuClicked)
+    }
+  }
+
+  const activeHandler = () => {
+    setIsActive('')
+    setIsMenuClicked(false)
+  }
+
+  console.log(isMenuClicked);
 
   Header.propTypes = {
     clickNavItem: PropTypes.func.isRequired
@@ -18,7 +38,7 @@ function Header(events) {
 
 
   return (<>
-    <div className="header">
+    <div className={`header ${isActive}`}>
       <div className="logo">
         <img alt="logo" src={logo} />
       </div>
@@ -28,11 +48,13 @@ function Header(events) {
             <Link to={el.to}
             key={i}
             className="nav-item"
-            onClick={() => clickNavItem(el)}
+            onClick={() => {clickNavItem(el); activeHandler()}}
             >{el.value}</Link>
           ))}
         </ul>
       </div>
+
+      <div onClick={updateMenu} className="burger__button">&#10005;</div>
     </div>
     </>
     )
